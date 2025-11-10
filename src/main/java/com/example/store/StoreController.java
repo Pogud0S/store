fc6db82c6e249feea0267153d1a863fd7608adcf
ivanz <ivanzhar1307@gmail.com>
2025-11-10 15:30:26 +0300

    sad

--- src/main/java/com/example/store/StoreController.java
+++ src/main/java/com/example/store/StoreController.java
@@ -1,4 +1,17 @@
 package com.example.store;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class StoreController {
+    @Value("${spring.application.name}")
+    private String name;
+
+    @RequestMapping("/")
+    String requestMapping(){
+        System.out.println(name);
+        return "index.html";
+    }
 }
